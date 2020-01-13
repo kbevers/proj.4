bfa9b00b21dd846d353d8b7527fb5222f2109c39
Sveinung Himle <himsve@kartverket.no>
2020-01-13 14:46:51 +0100

    Impl pj_cplist_merge()

--- src/cplist.cpp
+++ src/cplist.cpp
@@ -32,11 +32,56 @@
 static PJ_COMMONPOINTS *list = nullptr;
 #define PJ_MAX_PATH_LENGTH 1024
 
+static int pj_cplist_merge(projCtx ctx, const char *cp_name, PJ_COMMONPOINTS ***p_list, int *p_listcount, int *p_list_max)
+{
+	int match = 0;
+	PJ_COMMONPOINTS *this_cp, *tail = nullptr;
+	
+	for (this_cp = list; this_cp != nullptr; this_cp = this_cp->next)
+	{
+		if (strcmp(this_cp->cp_name, cp_name) == 0)
+		{
+			match = 1;
+
+			if (this_cp->cp == nullptr)
+				return 0;
+
+			if (*p_listcount >= *p_list_max - 2)
+			{
+				PJ_COMMONPOINTS **new_list;
+
+				int new_max = *p_list_max + 20;
+
+				new_list = (PJ_COMMONPOINTS **)pj_calloc(new_max, sizeof(void *));
+				
+				if (!new_list) 
+				{
+					pj_ctx_set_errno(ctx, ENOMEM);
+					return 0;
+				}
+				if (*p_list != nullptr)
+				{
+					memcpy(new_list, *p_list, sizeof(void *) * (*p_list_max));
+					pj_dalloc(*p_list);
+				}
+
+			}
+
+
+		}
+	}
+
+
+	// TODO: Recursive...
+
+	return 0;
+}
+
 PJ_COMMONPOINTS **pj_cplist(projCtx ctx, const char *lists, int *list_count)
 {
 	const char *s;
 	PJ_COMMONPOINTS **list = nullptr;
-	int grid_max = 0;
+	int list_max = 0;
 
 	pj_errno = 0;
 	*list_count = 0;
@@ -72,17 +117,17 @@ PJ_COMMONPOINTS **pj_cplist(projCtx ctx, const char *lists, int *list_count)
 		if (*s == ',')
 			s++;
 
-
-
-
+		if (!pj_cplist_merge(ctx, name, &list, list_count, &list_max) && required)
+		{
+			pj_dalloc(list);
+			pj_ctx_set_errno(ctx, PJD_ERR_FAILED_TO_LOAD_CPL);
+			pj_release_lock();
+			return nullptr;
+		}
+		else
+			pj_errno = 0;
 	}
+	pj_release_lock();
 
-	// TODO: Temporary 
 	return list;
-}
-
-static int pj_cplist_merge(projCtx ctx, const char *cp_name, PJ_COMMONPOINTS ***p_list, int *p_listcount)
-{
-	// TODO: Recursive...
-	return 0;
 }
