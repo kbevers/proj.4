09d2b0d219de8255f4b2c449cbf4c3d86c8e1d9a
Sveinung Himle <himsve@kartverket.no>
2020-05-19 11:42:54 +0200

    Custum grid sql

--- data/sql/grid_transformation_custom.sql
+++ data/sql/grid_transformation_custom.sql
@@ -84,3 +84,30 @@ INSERT INTO "grid_transformation" VALUES(
     NULL,
     'EPSG','8666','Geoid (height correction) model file','Icegeoid_ISN2016.gtx',
     NULL,NULL,NULL,NULL,NULL,NULL,NULL,0);
+
+-- Norway
+
+ INSERT INTO "grid_transformation" VALUES(
+    'PROJ','EPSG_4937_TO_EPSG_5776','ETRS89 to NN1954 height',
+    NULL,NULL,
+    'EPSG','9665','Geographic3D to GravityRelatedHeight (gtx)',
+    'EPSG','4937', -- source CRS (ETRS89)
+    'EPSG','5776', -- target CRS (NN1954 height)
+    'EPSG','1352', -- area of use: Norway - Onshore
+    NULL,
+     'EPSG','8666','Geoid (height correction) model file','href2008a.gtx',
+    NULL,NULL,NULL,NULL,NULL,NULL,NULL,0);
+
+
+ INSERT INTO "grid_transformation" VALUES(
+    'PROJ','EPSG_4937_TO_EPSG_5776','ETRS89 to NN1954 height',
+    NULL,NULL,
+    'EPSG','9665','Geographic3D to GravityRelatedHeight (gtx)',
+    'EPSG','4937', -- source CRS (ETRS89)
+    'EPSG','5776', -- target CRS (NN1954 height)
+    'EPSG','1352', -- area of use: Norway - Onshore
+    NULL,
+     'EPSG','8666','Geoid (height correction) model file','href2008a.gtx',
+    NULL,NULL,NULL,NULL,NULL,NULL,NULL,0);
+
+ 
