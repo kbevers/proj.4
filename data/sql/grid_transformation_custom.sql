3c3c86d4e4b2d4420a460dccda18fdcc7b2628b4
Sveinung Himle <himsve@kartverket.no>
2020-06-04 12:54:26 +0200

    Tif in sql

--- data/sql/grid_transformation_custom.sql
+++ data/sql/grid_transformation_custom.sql
@@ -90,34 +90,35 @@ INSERT INTO "grid_transformation" VALUES(
  INSERT INTO "grid_transformation" VALUES(
     'PROJ','EPSG_4937_TO_EPSG_5776','ETRS89 to NN1954 height',
     NULL,NULL,
-    'EPSG','9665','Geographic3D to GravityRelatedHeight (gtx)',
+    'EPSG','9665','Geographic3D to GravityRelatedHeight (tif)',
     'EPSG','4937', -- source CRS (ETRS89 3D)
     'EPSG','5776', -- target CRS (NN1954 height)
     'EPSG','1352', -- area of use: Norway - Onshore
     NULL,
-     'EPSG','8666','Geoid (height correction) model file','href2008a.gtx',
+     'EPSG','8666','Geoid (height correction) model file','href2008a.tif',
     NULL,NULL,NULL,NULL,NULL,NULL,NULL,0);
 
  INSERT INTO "grid_transformation" VALUES(
     'PROJ','EPSG_5776_TO_EPSG_5941','ETRS89 to NN2000 height',
     NULL,NULL,
-    'EPSG','9665','Geographic3D to GravityRelatedHeight (gtx)',
+    'EPSG','9665','Geographic3D to GravityRelatedHeight (tif)',
     'EPSG','4937', -- source CRS (ETRS89 3D)
     'EPSG','5941', -- target CRS (NN1954 height)
     'EPSG','1352', -- area of use: Norway - Onshore
     NULL,
-	'EPSG','8666','Geoid (height correction) model file','HREF2018B_NN2000_EUREF89.gtx',
+	'EPSG','8666','Geoid (height correction) model file','HREF2018B_NN2000_EUREF89.tif',
     NULL,NULL,NULL,NULL,NULL,NULL,NULL,0);
 	
 	-- TODO: Returns height - delta height and not delta height
 
   INSERT INTO "grid_transformation" VALUES(
      'PROJ','EPSG_5941_TO_EPSG_5776','NN1954 to NN2000 height',
+
      NULL,NULL, 
-	 'EPSG','1084', 'Vertical Offset by Grid Interpolation (gtx)',
+	 'EPSG','1084', 'Vertical Offset by Grid Interpolation (tif)',
      'EPSG','5941', -- source CRS (NN2000 height)
      'EPSG','5776', -- target CRS (NN1954 height)
      'EPSG','1352', -- area of use: Norway - Onshore
 	 NULL,
-	 'EPSG','8732','Vertical offset file','NNTrans2018B.gtx',	
+	 'EPSG','8732','Vertical offset file','NNTrans2018B.tif',	
 	 NULL,NULL,NULL,NULL,NULL,NULL,NULL,0);
